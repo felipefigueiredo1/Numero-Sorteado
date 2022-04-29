0170fbcd83ab7e6bfd3ed32b49c501aff84329ba
Felipe Figueiredo <felipefigueiredosal@gmail.com>
2022-04-28 22:59:36 -0300

    feat

---
+++ NumeroSorteado.php
@@ -0,0 +1,37 @@
+<?php
+
+class NumeroSorteado 
+{
+    public $sets;
+
+    public function pegandoNumeros($inicio, $final)
+    {
+        $array = range($inicio, $final);
+        shuffle($array);
+        $sets = array_chunk($array, 5);
+
+        $this->sets = $sets;
+    }
+
+    public function iterando()
+    {
+        echo "Numeros sorteados: ";
+        foreach ($this->sets as $set) {
+            echo implode(',' , $set) , "\n";
+        }
+
+        $this->numerosUnicos($set);
+    }
+
+    public function numerosUnicos($set)
+    {
+        $numerosUnicos = array_unique($set);
+        echo '<br>';
+        echo "Numeros que não se repetem: " . $unico = implode(", ",array_unique($numerosUnicos));
+        echo "<br>";
+    }
+}
+
+$obj = new NumeroSorteado;
+$obj->pegandoNumeros(1,10);
+$obj->iterando();
